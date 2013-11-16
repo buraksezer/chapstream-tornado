b2e80795321cb3196e438a44c13fa6b8d41895fe
Burak Sezer <bburaksezer@gmail.com>
2013-11-16 22:02:31 +0200

    add urls.py for url managing

---
+++ chapstream/urls.py
@@ -0,0 +1,15 @@
+from chapstream.api.misc import MainHandler
+from chapstream.api.user import ProfileHandler
+from chapstream.api.user import LoginHandler
+from chapstream.api.user import LogoutHandler
+from chapstream.api.user import RegisterHandler
+from chapstream.api.timeline import TimelineHandler
+
+URLS = [
+    (r"/", MainHandler),
+    (r"/profile", ProfileHandler),
+    (r"/login", LoginHandler),
+    (r"/logout", LogoutHandler),
+    (r"/register", RegisterHandler),
+    (r"/timeline-socket", TimelineHandler)
+]
